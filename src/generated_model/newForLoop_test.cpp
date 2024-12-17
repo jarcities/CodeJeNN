d24c1adfbe4b539939fb88e130e5a7b41a1e2dd2
Punani Whisperer <Hawaiian647@gmail.com>
2024-12-16 17:21:53 -0800

    new testing

--- src/generated_model/newForLoop_test.cpp
+++ src/generated_model/newForLoop_test.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <cmath>
 #include <chrono>
+#include <tbb/parallel_for.h> // Include TBB header
 #include "newForLoop.h" // change file name to desired header file
 
 using Scalar = double;
@@ -10,17 +11,18 @@ using Scalar = double;
 int main() {
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    for (int j = 0; j < 1000000; j++) {
+    tbb::parallel_for(0, 1000000, [](int j) {
         std::array<Scalar, 3> input = {Scalar(1 + j), Scalar(2 + j), Scalar(3 + j)}; 
         auto output = newForLoop<Scalar>(input);
 
+        // Uncomment the following block for debugging or output
         // std::cout << "Iteration " << j << " - Input: "
         //           << (1 + j) << ", " << (2 + j) << ", " << (3 + j) << " - Output: ";
         // for (const auto& val : output) {
         //     std::cout << val << " ";
         // }
         // std::cout << std::endl;
-    }
+    });
 
     auto end_time = std::chrono::high_resolution_clock::now();
     auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
