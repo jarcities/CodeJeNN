699bdfd4fc656f196ae437b3a0d895d2b90c47c6
jarcities <Hawaiian647@gmail.com>
2025-08-01 17:06:45 -0400

    with new "compare_to_A" loss function.

--- src/training/mlp_eigen_sparse.py
+++ src/training/mlp_eigen_sparse.py
@@ -27,12 +27,12 @@ NUM_SAMPLES = 997
 M = 202
 BATCH_SIZE = 1
 EPOCHS = 700
-HIDDEN_UNITS = 8
+HIDDEN_UNITS = 4
 LEARNING_RATE = 1e-3
 CLIP_NORM = 1.0
 VALIDATION_SPLIT = 0.3
 RANDOM_SEED = 42
-EPS = 1e-10 #16-20
+EPS = 1e-16 #16-20
 NEGATIVE_SLOPE = 1e-1 #1-2
 
 #input sparse
@@ -62,8 +62,8 @@ for i in range(NUM_SAMPLES):
     #     np.set_printoptions(threshold=np.inf)
     #     print(iA)
     #LU instead
-    L, U = sp.linalg.lu(A, permute_l=True) #with P in L
-    # P, L, U = sp.linalg.lu(A) #w/o P in L
+    # L, U = sp.linalg.lu(A, permute_l=True) #with P in L
+    P, L, U = sp.linalg.lu(A) #w/o P in L
     LU = np.tril(L, -1) + U
     if np.any(np.abs(np.diag(U)) <= 0.0): #check invertibility
         skipped += 1
@@ -73,13 +73,16 @@ for i in range(NUM_SAMPLES):
     # A = A[:, PERM][PERM, :] 
     A = A.ravel()
     X_list.append(A[mask_in])
+    # y_list.append(A[mask_out])
+
+    #apply permutation
     # LU = LU[:, PERM][PERM, :] 
     LU = LU.ravel()
     y_list.append(LU[mask_out])
 
     #add to data sample list
-    # X_list.append(A.ravel()[mask_in]) #take nonzero entries
-    # y_list.append(LU.ravel()[mask_out]) #take nonzero entries
+    # X_list.append(A.ravel()[mask_in])
+    # y_list.append(LU.ravel()[mask_out]) 
 
 print(f"Skipped {skipped} singular matrices")
 X = np.stack(X_list, axis=0) 
@@ -148,18 +151,53 @@ def diag_penalty(y_true, y_pred):
     diag = tf.abs(tf.linalg.diag_part(U))
     penalty = tf.reduce_sum(tf.square(tf.maximum(EPS - diag, 0.0)))
     return base + NEGATIVE_SLOPE * penalty  #tune
-@tf.function  #optimize
+@tf.function
 def compare_to_A(y_true, y_pred):
-    LU = tf.reshape(y_pred, [-1, M, M])
+    batch_size = tf.shape(y_pred)[0]
+    
+    # Reconstruct full LU matrix from sparse prediction
+    full_LU = tf.zeros([batch_size, M * M], dtype=y_pred.dtype)
+    mask_out_tf = tf.constant(mask_out, dtype=tf.bool)
+    lu_indices = tf.where(mask_out_tf)
+    lu_indices = tf.reshape(lu_indices, [-1])
+    lu_indices = tf.cast(lu_indices, tf.int32)
+    
+    batch_indices = tf.range(batch_size, dtype=tf.int32)
+    batch_indices = tf.repeat(batch_indices, OUTPUT_DIM)
+    sparse_indices = tf.tile(lu_indices, [batch_size])
+    scatter_indices = tf.stack([batch_indices, sparse_indices], axis=1)
+    y_pred_flat = tf.reshape(y_pred, [-1])
+    
+    full_LU = tf.scatter_nd(scatter_indices, y_pred_flat, [batch_size, M * M])
+    LU = tf.reshape(full_LU, [-1, M, M])
+    
+    # Decompose LU into L and U
     U = tf.linalg.band_part(LU, 0, -1)
     lower_all = tf.linalg.band_part(LU, -1, 0)
-    diag     = tf.linalg.band_part(LU, 0, 0)
+    diag = tf.linalg.band_part(LU, 0, 0)
     strict_lower = lower_all - diag
-    batch_size = tf.shape(LU)[0]
     I = tf.eye(M, batch_shape=[batch_size], dtype=LU.dtype)
     L = I + strict_lower
+    
+    # Reconstruct A_pred = L * U
     A_pred = tf.matmul(L, U)
-    A_true = tf.reshape(y_true, [-1, M, M])
+    
+    # Reconstruct true LU matrix from y_true to get original A
+    full_LU_true = tf.zeros([batch_size, M * M], dtype=y_true.dtype)
+    y_true_flat = tf.reshape(y_true, [-1])
+    full_LU_true = tf.scatter_nd(scatter_indices, y_true_flat, [batch_size, M * M])
+    LU_true = tf.reshape(full_LU_true, [-1, M, M])
+    
+    # Decompose true LU into L and U to get original A
+    U_true = tf.linalg.band_part(LU_true, 0, -1)
+    lower_all_true = tf.linalg.band_part(LU_true, -1, 0)
+    diag_true = tf.linalg.band_part(LU_true, 0, 0)
+    strict_lower_true = lower_all_true - diag_true
+    L_true = I + strict_lower_true
+    
+    # Get original A_true = L_true * U_true
+    A_true = tf.matmul(L_true, U_true)
+    
     return tf.reduce_mean(tf.keras.losses.mae(A_true, A_pred))
 
 #compile
@@ -167,8 +205,8 @@ opt = optimizers.Adam(learning_rate=LEARNING_RATE,
                       clipnorm=CLIP_NORM
                       )
 model.compile(optimizer=opt, 
-              loss=tf.keras.losses.logcosh
-            #   loss=compare_to_A
+            #   loss=tf.keras.losses.logcosh
+              loss=compare_to_A
             #   loss=diag_penalty
             #   loss=tf.keras.losses.mae
               )
